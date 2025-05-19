74417455ad868b3b701d52076657b68db78bd946
yassine <98836474+yassineb07@users.noreply.github.com>
2025-05-19 19:34:47 +0100

    Add capitalize function and related tests

--- testing-practice.js
+++ testing-practice.js
@@ -1,4 +1,8 @@
-function sum(a, b) {
-  return a + b;
-}
-export { sum };
+const capitalize = (string) => {
+  if (string === '') return '';
+  const firstLetterCapitalized = string.at().toUpperCase();
+  const restOfString = string.slice(1).toLowerCase();
+  return firstLetterCapitalized.concat(restOfString);
+};
+
+export { capitalize };
--- testing-practice.test.js
+++ testing-practice.test.js
@@ -1,5 +1,11 @@
-import { sum } from './testing-practice';
+import { capitalize } from './testing-practice';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('capitalize lower case string', () => {
+  expect(capitalize('dog')).toBe('Dog');
+});
+test('capitalize Empty string', () => {
+  expect(capitalize('')).toBe('');
+});
+test('capitalize Uppercase string', () => {
+  expect(capitalize('RABBIT')).toBe('Rabbit');
 });
